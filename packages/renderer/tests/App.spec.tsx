bc53dda69e4e4d8bfdff05626269aaf5cbc50d92
Elliott Blocha <eblocha@gmail.com>
2022-01-08 16:49:53 -0500

    migrate to react

---
+++ packages/renderer/tests/App.spec.tsx
@@ -0,0 +1,10 @@
+
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
+import {afterAll, beforeAll, expect, test} from 'vitest';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeDefined();
+});
