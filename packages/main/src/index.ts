64111516702e8563a2d2ab9e86256b378ea0eea4
Elliott Blocha <eblocha@gmail.com>
2022-01-08 16:57:34 -0500

    use react devtools instead of vue

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -38,12 +38,12 @@ app.whenReady()
 
 
 /**
- * Install Vue.js or some other devtools in development mode only
+ * Install React.js or some other devtools in development mode only
  */
 if (import.meta.env.DEV) {
   app.whenReady()
     .then(() => import('electron-devtools-installer'))
-    .then(({default: installExtension, VUEJS3_DEVTOOLS}) => installExtension(VUEJS3_DEVTOOLS, {
+    .then(({default: installExtension, REACT_DEVELOPER_TOOLS}) => installExtension(REACT_DEVELOPER_TOOLS, {
       loadExtensionOptions: {
         allowFileAccess: true,
       },
