bfe21af4363fa1cc5eb5ca92617d61483f6dbbd6
Elliott Blocha <eblocha@gmail.com>
2022-01-08 18:01:59 -0500

    Switch to prettier. editorconfig refuses to work in vscode

--- packages/renderer/tests/App.spec.tsx
+++ packages/renderer/tests/App.spec.tsx
@@ -1,7 +1,6 @@
-
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
-import {expect, test} from 'vitest';
+import { expect, test } from 'vitest';
 
 test('renders learn react link', () => {
   render(<App />);
