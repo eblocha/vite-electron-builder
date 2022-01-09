7d986afc7c9b6e2abb99d454db40b9ce8d310006
Elliott Blocha <eblocha@gmail.com>
2022-01-09 09:43:22 -0500

    clarify some jsdocs

--- packages/shared/src/types/callbacks.ts
+++ packages/shared/src/types/callbacks.ts
@@ -1,6 +1,6 @@
 import type { API, Implementation } from './util';
 
-/** This represents all the callbacks as the renderer sees them */
+/** This represents all the implementations of the callbacks in the main process */
 export type Callbacks<A extends API> = {
   [K in keyof A]: Implementation<A, K>;
 };
--- packages/shared/src/types/subscriptions.ts
+++ packages/shared/src/types/subscriptions.ts
@@ -1,6 +1,6 @@
 import type { Channels } from './channels';
 
-/** Runs when an event is recieved */
+/** Runs in the renderer when an event is recieved */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Callback<A extends any[]> = (...args: A) => void;
 
