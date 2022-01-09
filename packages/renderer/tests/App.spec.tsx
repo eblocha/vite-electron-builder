bc9c14b6fa0f7b7e67b9ba102bca590c2ca6b00c
Elliott Blocha <eblocha@gmail.com>
2022-01-08 19:46:43 -0500

    add cra eslint config

--- packages/renderer/tests/App.spec.tsx
+++ packages/renderer/tests/App.spec.tsx
@@ -1,9 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { test, expect } from '@jest/globals';
 import App from '../src/App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeDefined();
+  expect(linkElement).toBeInTheDocument();
 });
